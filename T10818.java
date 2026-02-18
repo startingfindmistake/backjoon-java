0260c5b01d7467eb2024c03fe2eadd97ab677835
Startingfindmistakesamsung <dudgh2328@gmail.com>
2026-02-18 19:48:02 +0900

    문제 해결 완료

--- T10818.java
+++ T10818.java
@@ -7,23 +7,28 @@ public class T10818 {
 
         // 숫자를 N번 받을지 입력을 받는다.
         int N = sc.nextInt();
-        // 이렇게 하면은 음수일 경우 대비를 하지 못한다.
-        int max = 0;
-
         int arr[] = new int[N];
 
         for(int i = 0; i < N; i ++){
-            int A = sc.nextInt();
-            arr[i] = A;
+            arr[i] = sc.nextInt();
         }
+
+        int max = arr[0];
+        int min = arr[0];
         
 
         for(int i = 0; i < N; i++){
-            if (arr[i] > arr[i+1]){
+            if (max < arr[i]){
                 max = arr[i];
             }
+
+            if (arr[i] < min){
+                min = arr[i];
+            }
+
+            
         }
-        System.out.println(max);
+        System.out.print(min + " " + max);
 
     }
 }
