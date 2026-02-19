7025dce7669b3f76fd3b0ae638a8eca89e041714
Startingfindmistakesamsung <dudgh2328@gmail.com>
2026-02-19 20:03:47 +0900

    T10810 문제에서 뛰어쓰기로 작성을 해도 잘 입력이 되는지 확인

---
+++ T10810.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class T10810{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        System.out.println(N);
+        System.out.println(M);
+
+        
+    }
+}
