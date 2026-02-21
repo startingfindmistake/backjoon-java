81e6ae06a862a31de0f1ac19e2fd705824e22e24
Startingfindmistakesamsung <dudgh2328@gmail.com>
2026-02-21 16:38:05 +0900

    한번에 결과값이 출력되도록 stringbuilder클래스를 활용하여 출력함

--- T9086.java
+++ T9086.java
@@ -11,6 +11,10 @@ public class T9086 {
         int count = 0;
         int B = 0;
 
+
+        // System.in이 필요 없음 왜냐하면 키보드에서 입력받을게 아니기 때문
+        StringBuilder sb = new StringBuilder();
+
         // 테스트 케이스를 T번 반복하여 입력 받는다.
         while(count < T){
 
@@ -20,13 +24,21 @@ public class T9086 {
             // 마지막 인덱스를 알기 위해서 인덱스의 길이를 구해야 함
             B = A.length();
             
-            System.out.print(""+A.charAt(0) + A.charAt(B-1));
+            sb.append(A.charAt(0));
+            sb.append(A.charAt(B-1));
+            sb.append("\n");
             
             count++;
         }
+
+        System.out.print(sb);
         //메모리 누수 및 중복 실행 방지
         sc.close();
 
         // 테스트 케이스중 첫번재와 마지막 인덱스를 출력한다.
     }
 }
+
+
+
+
