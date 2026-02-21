3659bdc723f43b433f4b680995bf1e0ce422ef1a
Startingfindmistakesamsung <dudgh2328@gmail.com>
2026-02-21 12:12:45 +0900

    새로운 함수 객체.length(); 함수 객체의 길이를 측정하고 싶을때 사용한다.

---
+++ T2743.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class T2743 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // 문자열 단어를 입력 받는다.
+        String A = sc.next();
+
+        // 문자열 길이를 출력해야 한다.
+        System.out.print(A.length());
+    }
+}
--- T27866.java
+++ T27866.java
@@ -5,8 +5,19 @@ public class T27866 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+
+        // 1. 문자열을 입력 받음
         String S = sc.next();
 
+        // 2. 몇번째 알파벳을 의미하는 정수를 입력받음
+        int i = sc.nextInt();
+
+        System.out.print(S.charAt(i - 1));
+
+
+
+        /*자바 문자열이 잘 출력이 되는지 확인
         System.out.print(S);
+        */
     }
 }
