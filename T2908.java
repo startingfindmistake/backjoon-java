19ec9cb3f2784ede6f00290b8e72c721ac61b9b6
Startingfindmistakesamsung <dudgh2328@gmail.com>
2026-03-03 19:35:44 +0900

    3자리 숫자를 두번 문자열로 입력받는걸로 수정을 완료했습니다.

---
+++ T2908.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.lang.StringBuilder;
+
+public class T2908 {
+    public static void main(String[] args){
+        // 입력을 받아와야 하기 때문에 Scanner 객체를 생성함
+        Scanner sc = new Scanner(System.in);
+        // 문자열을 뒤집기 위해서는 StringBuilder 객체가 필요함
+        StringBuilder sb = new StringBuilder();
+
+        //일단 3자리수 정수를 2개 입력받아야 한다.
+        String A = sc.next();
+        String B = sc.next();
+
+        // 그리고 그 정수를 각자 뒤집어야 한다.
+        // 뒤집은 정수를 다른 변수에 다시 저장한다. C, D
+
+        // 뒤집은 정수를 비교해보자(if문을 사용한다.)
+
+    }
+}
