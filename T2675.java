3056d15ce013aac524204a4807373a3ba92f9c57
Startingfindmistakesamsung <dudgh2328@gmail.com>
2026-03-03 14:52:45 +0900

    수정완료

--- T2675.java
+++ T2675.java
@@ -16,7 +16,6 @@ public class T2675 {
         for(int i = 0; i < T; i++){
             // 문자열 R번 반복 입력횟수를 입력받는다.
             int R = sc.nextInt();
-
             // 문자열을 입력 받는다.
             String B = sc.next();
 
@@ -24,7 +23,11 @@ public class T2675 {
             // 그런데 문자열에서 각 문자를 추출해서 반복해야 한다.
             // 문자열에서 각 문자를 추출하기 위해 반복문으로 문자를 하나씩 읽어 들여 반복한다.
             for(int j = 0; j < B.length(); j++){
-                sb.append(B);
+                char C = B.charAt(j);
+
+                for(int k = 0; k < R; k++){
+                    sb.append(C);
+                }
             }
             sb.append("\n");
         }
