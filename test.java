4df4b968c21af61d3ddb3c760f45b2857d6ba319
Startingfindmistakesamsung <dudgh2328@gmail.com>
2026-03-19 11:36:12 +0900

    for문 증감과 if문 헷갈려서 테스트로 결과값 확인

--- test.java
+++ test.java
@@ -1,8 +1,28 @@
-public class test {
-    public static void main(String[] args) {
-        String text = "HelloJava World! Java is fun.";
+import java.util.Scanner;
 
-        int indexJava = text.indexOf("Java");
-        System.out.println(indexJava);
-    }
+public class test {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        // charAt()메서드 자체가 하나의 긴 `string`안에서 특정 위치(인덱스)의 글자를 하나씩 빼오기 때문에 배열이 필요없음
+        String text = sc.next();
+        int i = 0;
+
+        // 반복문을 통해 하나씩 비교를 해보자
+        for(i = 0; i < text.length(); i++){
+            if(text.charAt(i) == 'c') {
+                if(text.charAt(i+1) == '=' || text.charAt(i+1) == '-'){
+                    // 맞다면 다음 글자로 숫자 카운트를 하고 그것을 건너띄어야 한다.
+                    i += 2;
+                    // 이렇게 하면 if문을 나오면 어디로 가지?
+                } else {
+                    System.out.println("2번째 안쪽 if문");
+                }
+            } else {
+                System.out.println(i);
+                System.out.println("1번째 if문");
+            }
+        } System.out.println("for문 밖");
+        System.out.println(i);
+    } 
 }
